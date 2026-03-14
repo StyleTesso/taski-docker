62f04e5330da163ca831b9501ee39bafc2791689
Максим Зиновьев <crystalmorfy@mail.ru>
2026-03-14 17:54:07 +0300

    14

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 """urls.py."""
-from django.contrib import admin
-from django.urls import include, path
 from rest_framework import routers
 from api import views
+
+from django.contrib import admin
+from django.urls import include, path
+
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
